a5f20381fe9bbbebb07e85c26620684c15e61513
poktok <143868561+luisvpena@users.noreply.github.com>
2024-11-24 19:35:40 -0600

    Add files via upload

---
+++ amplify/recipe-generator/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "project": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
